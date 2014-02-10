1e63244e7749aa8865c24c8e37e5fc773f9e0b1b
lgnlgn <gnliang10@126.com>
2014-02-10 09:53:48 +0800

    add paddle

---
+++ feluca-paddle/src/main/java/org/feluca/paddle/App.java
@@ -0,0 +1,13 @@
+package org.feluca.paddle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
