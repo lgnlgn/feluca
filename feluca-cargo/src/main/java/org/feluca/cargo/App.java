9f38cd4139424d5f6e42c5694899be11bfb6b855
lgnlgn <gnliang10@126.com>
2014-04-07 12:58:54 +0800

    cargo for data processing

---
+++ feluca-cargo/src/main/java/org/feluca/cargo/App.java
@@ -0,0 +1,13 @@
+package org.feluca.cargo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
